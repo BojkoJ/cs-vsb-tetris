39ffc8031407b3e9bd6e5efdde6aaa8bf0a8de65
Jan Bojko <honzabojko@seznam.cz>
2023-09-25 07:56:35 +0200

    Add project files.

---
+++ Tetris2/MultidimensionalArray.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris2
+{
+    internal class MultidimensionalArray
+    {
+        public void Run()
+        {
+            bool[,] data = new bool[10, 20];
+
+            T(2, 2, data);
+            Z(5, 7, data);
+
+            shi
+
+            MainLoop(data);
+
+            Console.WriteLine();
+        }
+
+        public void T(int x, int y, bool[,] data)
+        {
+            data[x, y] = true;
+            data[x + 1, y] = true;
+            data[x + 2, y] = true;
+            data[x + 1, y + 1] = true;
+
+            //tvar T
+        }
+
+        public void Z(int x, int y, bool[,] data)
+        {
+            data[x, y] = true;
+            data[x + 1, y] = true;
+            data[x + 1, y + 1] = true;
+            data[x + 2, y + 1] = true;
+
+            //tvar Z
+        }
+
+        public void MainLoop(bool[,] data)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            // \u2588 znak - 2 vedle sebe - kostička
+
+            // .Length vrátí šířku * výšku - počet hodnot v poli
+            // nás ale zajímá kolik jich je horizontálně a kolik vertikálně
+
+            // getLength(0) by měl vrátit horizontální rozměr
+            // getLength(1) by měl vrátit vertikální rozměr
+
+            Console.CursorVisible = false;
+
+            for(int i = 0; i < data.GetLength(1); i++)
+            {
+                for(int j = 0; j < data.GetLength(0); j++)
+                {
+                    Console.Write(data[j, i] ? "\u2588\u2588" : "  ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Tetris2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tetris2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MultidimensionalArray v1 = new MultidimensionalArray();
+            v1.Run();
+
+
+            Console.ReadKey();
+        }
+    }
+}
