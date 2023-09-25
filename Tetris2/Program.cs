8ac87165f073f4b2492b6102337cbace7a6394f4
Jan Bojko <honzabojko@seznam.cz>
2023-09-25 11:18:43 +0200

    Mode switching.

--- Tetris2/Program.cs
+++ Tetris2/Program.cs
@@ -4,14 +4,29 @@
     {
         static void Main(string[] args)
         {
-            //MultidimensionalArray v1 = new MultidimensionalArray();
-            //v1.Run();
+            int choice = 0;
+            Console.Write("Select 1-3 (1-Multidimensional Arr, 2-Jagged Arr, 3-Structure Arr) : ");
+            choice = int.Parse(Console.ReadLine());
 
-            //JaggedArray v2 = new JaggedArray();
-            //v2.Run();
+            switch(choice)
+            {
+                case 1:
+                    MultidimensionalArray v1 = new MultidimensionalArray();
+                    v1.Run();
+                    break;
+                case 2:
+                    JaggedArray v2 = new JaggedArray();
+                    v2.Run();
+                    break;
+                case 3:
+                    StructureArray v3 = new StructureArray();
+                    v3.Run();
+                    break;
+                default: 
+                    Console.WriteLine("You haven't selected any mode (1-3). Program ends now...");
+                    break;
 
-            StructureArray v3 = new StructureArray();
-            v3.Run();
+            }
 
             Console.ReadKey();
         }
